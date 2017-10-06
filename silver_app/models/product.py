a63d7af9c6f86abd85e603f1d2340120d7da4af1
Filip Slatinac <fslat092@uottawa.ca>
2017-10-05 23:07:33 -0400

    minor addition

--- silver_app/models/product.py
+++ silver_app/models/product.py
@@ -3,4 +3,5 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=30)
     image = models.URLField()
+    description = models.TextField()
     category = models.ForeignKey(Category)
