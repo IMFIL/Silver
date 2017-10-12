c4769a9995b74d40dc4f101d1526e7d153e19888
benmacdonald <ben@perpetualabs.com>
2017-10-11 20:45:09 -0700

    abstract

--- silver_app/abstract_classes/scrapers.py
+++ silver_app/abstract_classes/scrapers.py
@@ -4,15 +4,9 @@ from abc import ABC, abstractmethod
 class Scrapers(ABC):
 
     @abstractmethod
-    def __init__(self, search_term):
-        self.search_term = search_term
-
-    @abstractmethod
-    @property
     def products(self):
         pass
 
     @abstractmethod
-    @property
     def html(self):
         pass
